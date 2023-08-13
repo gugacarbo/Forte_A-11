8e4dfc8908d1119b71119328f5814c9a99452983
gugacarbo <guga_carbo@hotmail.com>
2023-08-13 17:00:51 -0300

    MPU Accelerometer

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
+#include <LoRa_E32.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include <Wire.h>
 #include <FS.h>
 #include <SD.h>
 #include <CircularBuffer.h>
+#include <MPU9250.h> 
 #include <TinyGPSPlus.h>
+// #include <SoftwareSerial.h>
 
-//! Implementar Lora
 
 //Defines
 #define LOG_MESSAGE false
@@ -18,22 +20,45 @@
 //Toggle Debug Serial Logs
 #define DEBUG DEBUG_VERBOSE
 
+//Sensors I2C (MPU, BMP280)
+#define I2C_SDA 21
+#define I2C_SCL 22
+
+//SD SPI
+#define SD_CS 5
+#define SD_MOSI 23
+#define SD_CLK 18
+#define SD_MISO 19
+
+//GPS Serial2 UART
+#define GPS_RX 16
+#define GPS_TX 17
+
+//Lora Serial0 Uart
+#define LORA_RX         3 //? Serial0 RX
+#define LORA_TX         1 //? Serial0 TX
+#define LORA_AuxPin     0 //?
+#define LORA_M0         0 //?
+#define LORA_M1         0 //?
+#define LORA_BAUDRATE   UART_BPS_RATE_9600
+
 //Fatal Errors Ids
 #define BMP_ERROR           1
 #define SD_MOUNT_ERROR      2
-#define NO_SD_ERROR         3
-#define DIR_CREATION_ERROR  4
-#define FILE_CREATION_ERROR 5
-#define FILE_WRITE_ERROR    6
-#define FILE_OPEN_ERROR     7
+#define MPU_ERROR           3
+#define NO_SD_ERROR         4
+#define DIR_CREATION_ERROR  5
+#define FILE_CREATION_ERROR 6
+#define FILE_WRITE_ERROR    7
+#define FILE_OPEN_ERROR     8
 
-//Config
-#define ROCKET_GOAL             500.0 //m
-//definir valor da altitude em que o foguete tem que passar para começar a contar o tempo
-#define FLIGHT_START_ALTITUDE   10.0  //m
-#define FLIGHT_START_AFTER      11000 //ms
+//Flight Config
+#define ROCKET_GOAL               500.0 //m
+#define FLIGHT_START_ALTITUDE     10.0  //m
+#define FLIGHT_START_AFTER        11000 //ms
 #define RECOVERY_DEPLOY_ALTITUDE  400.0 //m
-#define RECOVERY_PIN            4     //same as old servo (D4)
+#define RECOVERY_PIN              4     //same as old servo (D4)
+
 //Files
 #define ROOT_DIR           "/"
 #define FOLDER_NAME        "flight_record"  //ROOT_DIR + "/" + FOLDER_NAME
@@ -51,22 +76,22 @@
 #define DATA_INTERVAL_TIME      500 //ms
 #define ALTITUDE_INTERVAL_DELAY 200 //ms
 
+
+
 //Variables
 bool flightStarted = false;
 bool goalReached = false;
 bool recovering = false;
-
-unsigned long flightStarted_Time_offset = 0;
-
-unsigned long current_time = 0;
 float altitudeOffset = 0;
-
 float apogee = 0;
+unsigned long current_time = 0;
+unsigned long flightStarted_Time_offset = 0;
 
 //BPM
 float current_altitude;
 float current_temperature;
 float current_pressure;
+
 //Acelerometer
 float current_roll;
 float current_yaw;
@@ -77,6 +102,7 @@ float current_Z_acceleration;
 float current_GyroX;
 float current_GyroY;
 float current_GyroZ;
+
 //GPS
 char latitude[12];
 char longitude[12];
@@ -88,12 +114,16 @@ unsigned long altitudeInterval_time = 0;
 //Objects
 Adafruit_BMP280 bmp;
 TinyGPSPlus gps;
+MPU9250 mpu;
 
 File root;
 String logFilePath;
 String dataFilePath;
 String altitudeFilePath;
 CircularBuffer <float, 6> altitudeBuffer;
+
+LoRa_E32 LoraTransmiter(LORA_RX, LORA_TX, &Serial, LORA_AuxPin, LORA_BAUDRATE);
+
 
 //Functions
 void beep(int times = 1, int beeps_interval = 0, int time = BUZZER_BEEP_TIME);
@@ -115,6 +145,23 @@ void get_Temperature();
 void get_Coordenates();
 void get_Position();
 
+float get_GyroX();
+float get_GyroY();
+float get_GyroZ();
+float get_Roll();
+float get_Pitch();
+float get_Yaw();
+float get_AccX();
+float get_AccY();
+float get_AccZ();
+// float get_AccLinX();
+// float get_AccLinY();
+// float get_AccLinZ();
+// float get_MagX();
+// float get_MagY();
+// float get_MagZ();
+// float get_TempMPU();
+
 void set_AltitudeOffset();
 
 void handle_FlightStart();
@@ -125,6 +172,8 @@ bool isFalling();
 void activateRecovery();
 
 void handle_SerialLogData();
+void printCalibration();
+
 
 void setup() {
 #if DEBUG
@@ -132,8 +181,8 @@ void setup() {
 #endif
   Log("Setup start");
 
-  Serial2.begin(9600, SERIAL_8N1);
 
+  Serial2.begin(9600);
   pinMode(RECOVERY_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
 
@@ -185,7 +234,12 @@ void loop() {
 //Sensors
 void initSensors() {
 //INICIALIZAÇÃO SENSORES
-//BMP
+  //I2C
+  Wire.begin();
+
+//GPS
+  Serial2.begin(9600);
+  //BMP
   if (!bmp.begin(0x76)) {
     Log(F("Sensor BMP280 não foi identificado! Verifique as conexões."), LOG_ERROR);
     Error(BMP_ERROR);
@@ -202,17 +256,29 @@ void initSensors() {
   }
 
   //MPU
-  //initializeMpuModule();
-  /*Wire.begin();
-    if (!mpu.setup(0x68)) {
-    Serial.println("Mpu not working");
-    }
-    else {
-    mpu.verbose(true);
-    mpu.calibrateAccelGyro();
-    print_calibration();
-    mpu.verbose(false);
-    }*/
+  delay(2000);
+
+  MPU9250Setting setting;
+  setting.accel_fs_sel = ACCEL_FS_SEL::A16G;
+  setting.gyro_fs_sel = GYRO_FS_SEL::G2000DPS;
+  setting.mag_output_bits = MAG_OUTPUT_BITS::M16BITS;
+  setting.fifo_sample_rate = FIFO_SAMPLE_RATE::SMPL_200HZ;
+  setting.gyro_fchoice = 0x03;
+  setting.gyro_dlpf_cfg = GYRO_DLPF_CFG::DLPF_41HZ;
+  setting.accel_fchoice = 0x01;
+  setting.accel_dlpf_cfg = ACCEL_DLPF_CFG::DLPF_45HZ;
+
+
+
+  if (!mpu.setup(0x68, setting)) {
+    Log("MPU connection failed. Please check your connection with `connection_check` example.", LOG_ERROR);
+    Error(MPU_ERROR);
+  }
+
+  mpu.verbose(true);
+  mpu.calibrateAccelGyro();
+  printCalibration();
+  mpu.verbose(false);
 
 }
 
@@ -332,44 +398,18 @@ void get_Pressure() {
 //Get Positon (Accelerometer)
 //Aceleration/Rotation
 void get_Position() {
-   /*if (mpu.update())
-      {
-      current_roll = get_Roll();
-      Serial.print(current_roll);
-      Serial.print(", ");
-
-      current_pitch = get_Pitch();
-      Serial.print(current_pitch);
-      Serial.print(", ");
-
-      current_yaw = get_Yaw();
-      Serial.print(current_yaw);
-      Serial.print(", ");
-
-      current_X_acceleration = get_AccX();
-      Serial.print(current_X_acceleration);
-      Serial.print(", ");
-
-      current_Y_acceleration = get_AccY();
-      Serial.print(current_Y_acceleration);
-      Serial.print(", ");
-
-      current_Z_acceleration = get_AccZ();
-      Serial.print(current_Z_acceleration);
-      Serial.print(", ");
-
-      current_GyroX = get_GyroX();
-      Serial.print(current_GyroX);
-      Serial.print(", ");
-
-      current_GyroY = get_GyroY();
-      Serial.print(current_GyroY);
-      Serial.print(", ");
-
-      current_GyroZ = get_GyroZ();
-      Serial.print(current_GyroZ);
-      Serial.print(", ");
-      }*/
+  if (mpu.update())
+  {
+    current_roll = get_Roll();
+    current_pitch = get_Pitch();
+    current_yaw = get_Yaw();
+    current_X_acceleration = get_AccX();
+    current_Y_acceleration = get_AccY();
+    current_Z_acceleration = get_AccZ();
+    current_GyroX = get_GyroX();
+    current_GyroY = get_GyroY();
+    current_GyroZ = get_GyroZ();
+  }
 }
 
 //Get Location (GPS)
@@ -387,6 +427,61 @@ void get_Coordenates() {
     }
   }
 }
+
+
+// float get_AccLinX() {
+//   return mpu.getLinearAccX();
+// }
+// float get_AccLinY() {
+//   return mpu.getLinearAccY();
+// }
+// float get_AccLinZ() {
+//   return mpu.getLinearAccZ();
+// }
+
+float get_GyroX() {
+  return mpu.getGyroX();
+}
+float get_GyroY() {
+  return mpu.getGyroY();
+}
+float get_GyroZ() {
+  return mpu.getGyroZ();
+}
+
+// float get_MagX() {
+//   return mpu.getMagX();
+// }
+// float get_MagY() {
+//   return mpu.getMagY();
+// }
+// float get_MagZ() {
+//   return mpu.getMagZ();
+// }
+
+float get_Roll() {
+  return mpu.getRoll();
+}
+float get_Pitch() {
+  return mpu.getPitch();
+}
+float get_Yaw() {
+  return mpu.getYaw();
+}
+
+float get_AccX() {
+  return mpu.getAccX();
+}
+float get_AccY() {
+  return mpu.getAccY();
+}
+float get_AccZ() {
+  return mpu.getAccZ();
+}
+
+// float get_TempMPU() {
+//   return mpu.getTemperature();
+// }
 
 void set_AltitudeOffset() {
   Log("Definindo Altitude Atual:");
@@ -521,16 +616,75 @@ void appendToFile(fs::FS& fs, const char* path, const char* message) {
 //Serial Print Data
 void handle_SerialLogData() {
 #if DEBUG
+  //BPM
   Serial.print("Temperatura:");
   Serial.println(current_temperature);
+  Serial.print("Pressao: ");
+  Serial.println(current_pressure);
   Serial.print("Altitude: ");
   Serial.println(current_altitude);
+
   Serial.print("Latitude: ");
-  Serial.print(latitude);
-  Serial.print(F(", "));
+  Serial.println(latitude);
   Serial.print("Longitude: ");
-  Serial.print(longitude);
-  Serial.println(F(", "));
+  Serial.println(longitude);
+
+  Serial.print("Roll: ");
+  Serial.println(current_roll);
+  Serial.print("Pitch: ");
+  Serial.println(current_pitch);
+  Serial.print("Yaw: ");
+  Serial.println(current_yaw);
+#endif
+
+#if DEBUG == DEBUG_VERBOSE
+  Serial.print("X Acceleration: ");
+  Serial.println(current_X_acceleration);
+  Serial.print("Y Acceleration: ");
+  Serial.println(current_Y_acceleration);
+  Serial.print("Z Acceleration: ");
+  Serial.println(current_Z_acceleration);
+  Serial.print("Gyro X: ");
+  Serial.println(current_GyroX);
+  Serial.print("Gyro Y: ");
+  Serial.println(current_GyroY);
+  Serial.print("Gyro Z: ");
+  Serial.println(current_GyroZ);
+#endif
+}
+
+//Print Accelerometer Calibration
+void printCalibration() {
+#if DEBUG == DEBUG_VERBOSE
+  Serial.println("< MPU Calibration Parameters >");
+  Serial.println("Accel Bias [g]: ");
+  Serial.print(mpu.getAccBiasX() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+  Serial.print(", ");
+  Serial.print(mpu.getAccBiasY() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+  Serial.print(", ");
+  Serial.print(mpu.getAccBiasZ() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+  Serial.println();
+  Serial.println("Gyro Bias [deg/s]: ");
+  Serial.print(mpu.getGyroBiasX() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+  Serial.print(", ");
+  Serial.print(mpu.getGyroBiasY() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+  Serial.print(", ");
+  Serial.print(mpu.getGyroBiasZ() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+  Serial.println();
+  Serial.println("Mag Bias [mG]: ");
+  Serial.print(mpu.getMagBiasX());
+  Serial.print(", ");
+  Serial.print(mpu.getMagBiasY());
+  Serial.print(", ");
+  Serial.print(mpu.getMagBiasZ());
+  Serial.println();
+  Serial.println("Mag scale []: ");
+  Serial.print(mpu.getMagScaleX());
+  Serial.print(", ");
+  Serial.print(mpu.getMagScaleY());
+  Serial.print(", ");
+  Serial.print(mpu.getMagScaleZ());
+  Serial.println();
 #endif
 }
 
